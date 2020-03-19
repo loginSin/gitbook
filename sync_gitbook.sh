cbb098514c49ae6c8545b9f7506b7f8c2823d238
qixinbing <qixinbing@ronglcloud.cn>
2020-03-20 00:36:47 +0800

    msg

---
+++ sync_gitbook.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "** gitbook build start"
+
+gitbook build
+
+message=`git show -s --format=%s`
+
+
+echo "** copy doc to gitbook"
+
+cp -rf _book/* ../gitbook/
+
+
+echo "** push doc to github"
+
+cd ../gitbook
+
+git add .
+
+git commit -m $message
+
+git push
